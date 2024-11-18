5b040c5f86d8e744eb65102f8fb94f00f7d4b8c8
Manisha-mca <puppalamanisha2003@gmail.com>
2024-11-18 20:51:32 +0530

    Add files via upload

---
+++ day5/Addition.java
@@ -0,0 +1,47 @@
+Class Addition
+{
+int a,b,c;
+parteci
+
+Class Addition
+
+int a,b,c,
+
+void add ()
+
+a = 10;
+
+6=20;
+
+C = a+b;
+
+ect
+
+C
+
+system out Print In (The addition value is!
+
+3
+
+class Main
+
+{
+
+Public static Void main (string[] args){
+
+Addition a, = new addition();
+
+a, .add().oid add ()
+{
+a = 10;
+b=20;
+C= a+b;
+System.out.Println("The addition value is:"+c);
+}
+class Main
+{
+Public static Void main (String[] args){
+Addition a1 = new Addition();
+a1.add();
+}
+}
